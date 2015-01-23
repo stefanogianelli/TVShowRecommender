99be6e3ef9b701346fb20269bd4df76719969296
stefanogianelli <stefano.gianelli@gmail.com>
2015-01-23 23:17:15 +0100

    Completata creazione matrice S

--- manageDataset.jl
+++ manageDataset.jl
@@ -1,4 +1,5 @@
 #ricavo il percorso base da cui caricare i dataset
+#N.B.: pwd ritorna la cartella utente!
 path = string(pwd(),"\\TVShowRecommender")
 
 #carico la matrice con le informazioni sui programmi di training
@@ -29,6 +30,7 @@ training = training[:,[6,7,9]]
 
 #rimuovo i programIdx duplicati e sommo le loro durate
 #considero solo i programmi selezionati nel vettore ids
+#vengono inoltre rimosse le settimane 14 e 19
 trainingSize = size(training)[1]
 i = 1
 while i <= trainingSize
@@ -37,12 +39,14 @@ while i <= trainingSize
     #verifico che il programId corrente sia presente nel vettore ids
     if in(training[i,2], ids)
       #verifico se il programId non sia già presente
-      index = exixstProgramId(training[i,2], training[:,2], i)
+      index = findElem(training[i,2], training[:,2], i)
       if index != -1
+        #se il programId esiste viene sommato il valore della duration
         training[index,3] += training[i,3]
         training = training[[1:(i-1), (i+1):end], :]
         trainingSize -= 1
       else
+        #altrimenti passo alla riga successiva
         i += 1
       end
     else
@@ -57,19 +61,47 @@ while i <= trainingSize
   end
 end
 
-#calcolo la matrice S tramite cosine similarity
+#creo un vettore con la lista degli utenti
+users = Int64[]
+
+for i=1:size(training)[1]
+  #verifico se l'userId corrente non sia già stato inserito
+  if !in(training[i,1], users)
+    push!(users, training[i,1])
+  end
+end
+
+#preparo la URM
+URM = zeros(size(users)[1], size(ids)[1])
+for i=1:size(training)[1]
+  #cerco la riga corrispondente all'utente corrente
+  row = findElem(training[i,1], users, size(users)[1])
+  #cerco la colonna corrispondente al programma corrente
+  col = findElem(training[i,2], ids, size(ids)[1])
+  #posizione la durata nella posizione (row,col)
+  URM[row,col] = training[i,3]
+end
+
+#calcolo la matrice S tramite adjusted cosine similarity
+S = ones(size(ids)[1], size(ids)[1])
+for i=1:size(URM)[2]
+  for j=i+1:size(URM)[2]
+    #sfrutto la simmetria della matrice S per il calcolo della similarità
+    S[i,j] = S[j,i] = cosineSimilarity(URM[:,i], URM[:,j])
+  end
+end
 
 #calcolo la matrice C
 #importare qui il codice del file buildMatrixC
 
-#esporto il nuovo dataset di training
+#esporto il nuovo dataset di training?
 #writecsv("$path\\dataset\\training.csv", dataset)
 
 #=
 Controlla se l'id esiste già nel vettore "array", nell'intervallo da 1 a size
 Ritorna il numero di riga in cui è stato trovato l'id, -1 altrimenti
 =#
-function exixstProgramId (id, array, size)
+function findElem (id, array, size)
   for i = 1:(size-1)
     if array[i] == id
       return i
@@ -78,6 +110,40 @@ function exixstProgramId (id, array, size)
   return -1
 end
 
-#calcola la cosine similarity tra due vettori?
-function cosineSimilarity ()
+#calcola la cosine similarity tra due vettori
+function cosineSimilarity (a, b)
+  dim = size(a)[1]
+  #calcolo il numeratore
+  num = 0
+  #calcolo il denominatore
+  den = 0
+  den1 = 0
+  den2 = 0
+  #eseguo tutti i conti in un unico ciclo
+  for n=1:dim
+    #controllo che l'utente n abbia valutato entrambi gli item
+    if a[n] != 0 && b[n] != 0
+      #calcolo la media dei rating dati dall'utente corrente
+      avg = userAverage(n)
+      num += (a[n] - avg)*(b[n] - avg)
+      den1 += (a[n] - avg)^2
+      den2 += (b[n] - avg)^2
+    end
+  end
+  den = sqrt(den1)*sqrt(den2)
+  #restituisco la similarità totale
+  return num / den
+end
+
+#calcola la media dei ratings dati dall'utente n
+function userAverage (n)
+  tot = 0
+  count = 0
+  for j=1:size(URM)[2]
+    if URM[n,j] != 0
+      tot += URM[n,j]
+      count += 1
+    end
+  end
+  return tot / count
 end
