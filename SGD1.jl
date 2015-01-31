31bf6ff4dd2f7f9656a93299491c7df5bd37c2e7
stefanogianelli <stefano.gianelli@gmail.com>
2015-01-31 12:23:26 +0100

    Portato codice SGD nel caso delle matrici

--- SGD1.jl
+++ SGD1.jl
@@ -10,27 +10,32 @@ C = ones(length(ids),length(ids))
 Q = transpose(C)*S*C
 T = transpose(C)*C
 
+#tolleranza
+tol = 1e-6
+
 #numero massimo iterazioni
-miter = 100
+miter = 10000
 
 #dimensione passo
 #ottimizzazione: non fissarlo, ma dedurlo ad ogni passaggio
-alpha = 0.2
+alpha = 0.002
 
 #inizializzo la matrice M a 1
-M = ones(length(ids),length(ids))
+M = zeros(length(ids),length(ids))
 
 #calcolo la matrice M ottimale
-for i=1:miter
-  M = M - alpha*grad(M)
+i = 1
+while i <= miter && object(M) > tol
+  @printf "-----------------------\niter = %d\nF = %f\n" i object(M)
+  M = M - alpha * grad(M)
+  i += 1
 end
 
 M
 
 #definisco la funzione obiettivo
 function object (X::Matrix)
-  #TODO: scrivere funzione obiettivo
-  return 0;
+  return vecnorm(S - C * X * transpose(C))^2;
 end
 
 #restituisce il gradiente della funzione obiettivo
